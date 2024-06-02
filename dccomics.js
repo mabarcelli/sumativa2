aaff7c92b9c4d1a0e1bc1b588d2bad014192538e
mabarcelli <ma.barcelli@duocuc.cl>
2024-06-02 16:13:13 -0400

    comit 2 api

--- dccomics.js
+++ dccomics.js
@@ -1,69 +1,80 @@
 const dccomics = [
     {
-    "id":1,
+    "id":11,
     "name":"batman tres jokers",
     "groups": "liga de la justicia",
+    "price": "19.990",
     "img":"https://upload.wikimedia.org/wikipedia/en/7/77/Three_Jokers.jpeg"
     },
     {
-        "id":2,
+        "id":12,
         "name":"  batman catwoman",
         "groups":"liga de la justicia",
+        "price": "9.990",
         "img":"https://content.eccediciones.com//productos/12975/Batman_Catwoman_9_1a_cubierta_DEF_350.jpeg"  
     },
     {
-        "id":3,
+        "id":13,
         "name":"   noches oscuras : death metal vol 1",
         "groups":"liga de la justicia",
+        "price": "11.990",
         "img":"https://content.eccediciones.com/productos/18454/Portada_Noches_oscuras_death_metal_integral1.jpg"  
     },
 
     {
-        "id":4,
+        "id":14,
         "name":"liga de la justicia: dia del juicio",
         "groups":"liga de la justicia",
+        "price": "18.000",
         "img":"https://static.wikia.nocookie.net/marvel_dc/images/3/31/Day_of_Judgment_1.jpg/revision/latest?cb=20081018014846"  
     },
     {
-        "id":5,
+        "id":15,
         "name":"beast boy",
         "groups":"teen titans",
+        "price": "19.000",
         "img":"https://m.media-amazon.com/images/I/81ptH5qoCZL._AC_UF1000,1000_QL80_.jpg"  
     },
     {
-        "id":6,
+        "id":16,
         "name":"A judas among us",
         "groups":"teen titans",
+        "price": "15.980",
         "img":"https://prodimage.images-bn.com/pimages/9781401277598_p0_v2_s192x300.jpg"  
     },
     {
-        "id":7,
+        "id":17,
         "name":"damian knows best",
         "groups":"teen titans",
+        "price": "19.990",
         "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzckpVYXL1NlLJFG7WCX7weAYUxLvQHKjh9A&s"  
     },
     {
-        "id":8,
+        "id":18,
         "name":"pura maldad: lex luthor",
         "groups":"la legion del mal ",
+        "price": "29.990",
         "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWJU7XKweFdUP_1QOEFLHgKCWqYJ99Rt8ALw&s"  
     },
     {
-        "id":9,
+        "id":19,
         "name":"fuerza x",
         "groups":"escuadron suicida ",
+        "price": "9.990",
         "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwSVv37c5GSi4LwnKyIBz8AU1YmzkUi64A4Q&s"  
     },
     {
-        "id":10,
+        "id":20,
         "name":"Bad blood",
         "groups":"escuadron suicida",
+        "price": "15.990",
         "img":"https://content.eccediciones.com/productos/13325/cubierta_EscuadronSuicida_Mala_Sangre.jpg"  
     },
     {
-        "id":11,
+        "id":21,
         "name":"Heroes en crisis",
         "groups":" liga de la justicia",
+        "price": "10.990",
         "img":"https://cdnx.jumpseller.com/shazam-online/image/12960159/resize/255/255?1657143518"  
     }
 
